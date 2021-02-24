f0505b265efaffcd202f8bd4324c54da062cfea0
Shubham Mani Tripathi <70857701+tripathishubh@users.noreply.github.com>
2021-02-24 23:16:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>online voting</title>
+<link rel="stylesheet" href="css/bootstrap.css" />
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+   
+    <div id="logo" class="fl_left">
+      <h1><a href="#">ONLINE VOTING SYSTEM</a></h1>
+    </div>
+   
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="admin/index.php">Admin</a></li>
+        <li><a class="drop" href="#">Voter Panel</a>
+          <ul>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="registeracc.php">Registration</a></li>
+            
+          </ul>
+        </li>
+        
+    <li><a class="drop" href="#">Candidate Panel</a>
+          <ul>
+            <li><a href="candidatelogin.php">Login</a></li>
+            <li><a href="candidateregister.php">Registration</a></li>
+            
+          </ul>
+        </li>
+    
+      </ul>
+    </nav>
+  
+  </header>
+</div>
+
+
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/youth.jpg');">
+  <section id="testimonials" class="hoc container clear"> 
+   
+    <h2 class="font-x3 uppercase btmspace-80 "<a href="#"> YOUR VOTE IS YOUR VOICE</a></h2>
+  <div class="container">
+  <div class="col-sm-8 col-sm-offset-2" >
+    <div class="news"><h2><marquee>New polls are up and running. But they will not be up forever! Just Login and then go to Current Polls to vote for your favourate candidates. </marquee></h2></div>
+  </div>
+  </div>
+    <blockquote>
+  <ul class="nospace group">
+      <li class="one_half"><marquee behavior="scroll" direction="up">
+        This is an online voting system which can be used by candidate, voter, organization or admin to facilitate elections, right from the comfort of your home, which are presently conducted by Election Commission Of India (ECI).<br></br>Your vote remains anonymous as our system design strictly separates your personal data.<br></br>Have a Vision? Make The Right Decision! #VOTE!
+    </marquee></li> 
+    <br>
+    
+    </ul>
+
+        
+  </section>
+
+                              
+</div>
+ <div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+ 
+    <div class="one_third first">
+      <h6 class="title">Address</h6>
+      <ul class="nospace linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+         
+          <p>
+          Name        : Mr. Shubham Mani <br>
+          University  : JAMIA HAMDARD <br>
+          Batch       : 2016-19 <br>
+          Dept        : CSE <br>
+          </p>
+          </address>
+        </li>
+      </ul>
+    </div>
+
+    <div class="one_third">
+      <h6 class="title">Phone</h6>
+      <ul class="nospace linklist contact">
+       
+        <li><i class="fa fa-phone"></i> +91 9650469951<br>
+          +91 7065448544</li>
+
+
+      </ul>
+    </div>
+
+    <div class="one_third">
+      <h6 class="title">Email</h6>
+      <ul class="nospace linklist contact">
+        
+        <li><i class="fa fa-envelope-o"></i> shubhamt6661@gmail.com </li>
+     <li><i class="fa fa-envelope-o"></i> vmt661@gmail.com </li>
+
+      </ul>
+    </div>
+
+
+
+  </footer>
+</div>
+
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+  
+    <p class="fl_left">Copyright &copy; 2019 - All Rights Reserved - <a href="#">Mr. Shubham</a></p>
+    <p class="fl_right">Developed for <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">Software Engineering Project</a></p>
+   
+  </div>
+</div>
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script language="JavaScript" src="js/user.js">
+</script>
+<script type="layout/scripts/bootstrap.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+</body>
+</html>
