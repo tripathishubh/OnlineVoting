1dbf2aae6bdf384f0093765370be8e66edc69626
Shubham Mani Tripathi <70857701+tripathishubh@users.noreply.github.com>
2021-04-24 19:28:26 +0530

    Delete save.php

--- save.php
+++
@@ -1,9 +0,0 @@
-<?php
-		require_once('connection.php');
-		$vote = $_REQUEST['vote'];
-
-
-		$mysqli->query("UPDATE tbCandidates SET candidate_cvotes=candidate_cvotes+1 WHERE candidate_name='$vote'");
-
-		mysqli_close($mysqli);
-?> 
